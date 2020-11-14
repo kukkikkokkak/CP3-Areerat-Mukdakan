0d94d294ef9d90594c430f8b997292f5688d9ecb
kukkikkokkak <m.areerat.kk@gmail.com>
2020-11-14 13:00:01 +0700

    Exercise8_Areerat_M

---
+++ assignments/Exercise8_Areerat_M.py
@@ -0,0 +1,19 @@
+username = input("username : ")
+password = input("password : ")
+if username == "Member" and password == "1234":
+    print("-----Wellcome Member-----")
+    print("-----Choose your order-----")
+    print("apple    15 THB/piece")
+    print("banana   5  THB/piece")
+    print("orange   10 THB/piece")
+    print("-----Add to cart-----")
+    QuantityApple = int(input("Quantity of apple : "))
+    QuantityBanana = int(input("Quantity of banana : "))
+    QuantityOrange = int(input("Quantity of orange : "))
+    PriceApple = 15 * QuantityApple
+    PriceBanana = 5 * QuantityBanana
+    PriceOrange = 10 * QuantityOrange
+    total = PriceApple + PriceBanana + PriceOrange
+    print("Total amount : ", total)
+else:
+    print("!!!you are not allowed!!!")
